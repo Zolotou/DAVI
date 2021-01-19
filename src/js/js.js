6e030a80afc3e5b93aa41ce54975e37861a91fca
artur <me@zalart.com>
2021-01-20 01:00:26 +0300

    Created reference block for JS Section, Started to fill the reference content

--- src/js/js.js
+++ src/js/js.js
@@ -1,29 +1,65 @@
-
-
 const caseLibrary = [
   {
     section: 'JavaScript Basics',
-    title: 'Hello world! Embedding JavaScript',
-    html: '<p>You can include a JavaScript in your HTML just adding special tags: &lt;script&gt;&lt;/script&gt;</p><p>Any JavaScript code inside these tags will be executed by the client\'s browser</p>',
-    css: '',
-    js: 'var greet = "Hello World!"; document.write(greet); // Prints: Hello World!'
-  },
-  {
-    section: 'JavaScript Basics',
-    title: 'Inlining JavaScript',
-    html: '<p>You can insert JavaScript inside HTML tag</p><button onclick="alert(\'Hello World!\')">Click Me</button>',
+    checked: 'html',
+    title: 'Adding JavaScript to Your Web Pages',
+    reference: '<h3>Adding JavaScript to Your Web Pages</h3><p>There are typically three ways to add JavaScript to a web page:</p><ul><li>Embedding the JavaScript code between a pair of <code>&lt;script&gt;</code> and <code>&lt;/script&gt;</code> tag.</li><li>Creating an external JavaScript file with the <code>.js</code> extension and then load it within the page through the <code>src</code> attribute of the <code>&lt;script&gt;</code> tag.</li><li>Placing the JavaScript code directly inside an HTML tag using the special tag attributes such as <code>onclick</code>, <code>onmouseover</code>, <code>onkeypress</code>, <code>onload</code>, etc.</li></ul>',
+    html: `<h3>You can include a JavaScript in your HTML just adding special tags: </h3>
+    <script>
+      var greet = "Hello World!";document.write(greet); // Prints: Hello World!
+      </script>
+      <p>Any JavaScript code inside these tags will be executed by the client\'s browser</p>
+      <h3>Placing the JavaScript code directly inside an HTML tag:</h3>
+      <p>The special tag attributes such as onclick, onmouseover, onkeypress, onload allow to execute JavaScript included in HTML tags.</p>
+      <button onclick="alert(\'Hello World!\')">Click Me</button>
+      <h3>Loading an external JavaScript file:</h3>
+      <p>You can also create an external JavaScript file with the .js extension and then load it within the page through the src attribute of the &lt;script&gt; tag.</p>
+      <code>&lt;script src="/src/js/tutorial/hello.js"&gt;&lt;/script&gt;</code>`,
     css: '',
     js: ''
   },
   {
     section: 'JavaScript Basics',
-    title: 'Starting point',
+    checked: 'js',
+    title: 'JavaScript Variables',
+    reference: `<h3>What is Variable?</h3><p>Variables are used to store data, like string of text, numbers, etc. The data or value stored in the variables can be set, updated, and retrieved whenever needed. In general, variables are symbolic names for values.</p><p>You can create a variable with the <code>var</code> keyword, whereas the assignment operator (<code>=</code>) is used to assign value to a variable, like this: <code>var varName = value;</code></p><h3>The let and const Keywords</h3><p>ES6 introduces two new keywords <code>let</code> and <code>const</code> for declaring variables.</p><p>The <code>const</code> keyword works exactly the same as <code>let</code>, except that variables declared using <code>const</code> keyword cannot be reassigned later in the code.</p><h3>Naming Conventions for JavaScript Variables</h3><p>These are the following rules for naming a JavaScript variable:</p><ul>
+    <li>A variable name must start with a letter, underscore (<code>_</code>), or dollar sign (<code>$</code>).</li>
+    <li>A variable name cannot start with a number.</li>
+    <li>A variable name can only contain alpha-numeric characters (<code>A-z</code>, <code>0-9</code>) and underscores.</li>
+    <li>A variable name cannot contain spaces.</li>
+<li>A variable name cannot be a JavaScript keyword or a JavaScript reserved word.</li>
+</ul>`,
+    html: '',
+    css: '',
+    js: `// Declaring variables
+    let name = "Harry Potter";
+    document.write(name + "\\n");
+
+    let age = 11;
+    document.write(age + "\\n");
+    
+    let isStudent = true;
+    document.write(isStudent + "\\n");
+    
+    // Declaring constant
+    const PI = 3.14;
+    document.write(PI + "\\n"); // 3.14
+    
+    // Trying to reassign
+    PI = 10; // error`
+  },
+  {
+    section: 'JavaScript Basics',
+    checked: 'js',
+    title: 'JavaScript Output Methods',
+    reference: '<h3>Generating Output</h3><p>In JavaScript there are several different ways of generating output including writing output to the browser window or browser console, displaying output in dialog boxes, writing output into an HTML element, etc. Lets take a closer look at each of these techniques:</p><ul><li>Writing Output to Browser Console</li></ul>',
     html: "<p>К блоку ниже применятся анимации для свойств: width, height, background-color, transform. Наведите на него, чтоб увидеть, как они анимируются.</p><p>Клик по элементу приведёт к срабатыванию EventListener и обработке события 'click'. </p><div class='box'></div>",
     css: '.box {border - style: solid; border-width: 1px; display: block; margin: 20px; width: 100px; height: 100px; background-color: #0000FF; -webkit-transition: width 2s, height 2s, background-color 2s, -webkit-transform 2s; transition: width 2s, height 2s, background-color 2s, transform 2s; } .box:hover {background - color: #FFCCCC; width: 200px; height: 200px; -webkit-transform: rotate(360deg); transform: rotate(360deg); }',
     js: 'const box = document.querySelector(".box"); box.onclick = () => {box.style.backgroundColor = "green"; box.innerHTML = "<span style=\'color:white; font-size: 1.7rem\'>Зеленый квадрат Залевского</span>\"; }',
   },
   {
     section: 'JavaScript Basics',
+    checked: 'js',
     title: 'Second point',
     html: "<p>Привет, Мир! </p><div class='box'></div>",
     css: '.box {border-style: solid; border-width: 1px; display: block; margin: 20px; width: 100px; height: 100px; background-color: #0000FF; -webkit-transition: width 2s, height 2s, background-color 2s, -webkit-transform 2s; transition: width 2s, height 2s, background-color 2s, transform 2s; } .box:hover {background-color: #FFCCCC; width: 200px; height: 200px; -webkit-transform: rotate(360deg); transform: rotate(360deg); }',
@@ -31,6 +67,7 @@ const caseLibrary = [
   },
   {
     section: 'JavaScript Functions',
+    checked: 'js',
     title: 'Functions',
     html: "Functions",
     css: '.box {border-style: solid; border-width: 1px; display: block; margin: 20px; width: 100px; height: 100px; background-color: #0000FF; -webkit-transition: width 2s, height 2s, background-color 2s, -webkit-transform 2s; transition: width 2s, height 2s, background-color 2s, transform 2s; } .box:hover {background-color: #FFCCCC; width: 200px; height: 200px; -webkit-transform: rotate(360deg); transform: rotate(360deg); }',
@@ -134,6 +171,13 @@ function listCases(cases) {
 function appendSandboxCase(jsonData, title) {
   for (let key of jsonData) {
     if (key.title === title) {
+      let selectedRadio;
+      selectedRadio = key.checked;
+
+      document.querySelector(`#${selectedRadio}Editor`).checked = true;
+      document.querySelector('.reference').innerHTML = key.reference;
+
+
       htmlEditor.setValue(html_beautify(key.html));
       htmlEditor.refresh();
       cssEditor.setValue(css_beautify(key.css));
@@ -147,7 +191,12 @@ listCases(caseLibrary);
 const triggerCase = document.querySelectorAll('.instance');
 triggerCase.forEach(element => {
   element.addEventListener('click', () => {
+    const selectedInstances = document.querySelectorAll('.selected');
     appendSandboxCase(caseLibrary, element.textContent);
+    selectedInstances.forEach(instance => {
+      instance.classList.remove("selected");
+    })
+    element.classList.add("selected");
   })
 });
 
